63535e3322e054cd6703b6a8695fb978819308e2
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-20 17:45:48 +0300

    Checking whether a tile is in stock

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -1,6 +1,7 @@
 export function addTileFromStack(tile, tiles) {
-    console.log(tile, tiles)
-    if (tile.classList[1] === "sky"){
-        tile.classList.replace("sky", tiles)
-    }
+  console.log(tile, tiles);
+  if (tiles["sum"] > 0 && tile.classList[1] === "sky") {
+    tile.classList.replace("sky", tiles["type"]);
+    tiles["sum"]--;
+  }
 }
