cbb2e6bc59c9ed49a0f8a505d2b4bfd5406bbf89
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-20 12:48:24 +0300

    tiles structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
-const main = document.querySelector("main")
+const main = document.querySelector("main");
 
 for (let index = 0; index < 3000; index++) {
-    const div = document.createElement("div")
-    div.classList.add("tile", "sky")
-    div.id = `tile${index}`
-    main.append(div)
+  const div = document.createElement("div");
+  div.classList.add("tile", "sky");
+  div.id = `tile${index}`;
+  main.append(div);
 }
 
-function ceangeTiles(start, end, tile){
-    for (let index = start; index < end +1 ; index++) {
-        const div = document.querySelector(`#tile${index}`)  
-        div.classList.replace("sky", tile)      
-    }
+function ceangeTiles(start, end, tile) {
+  for (let index = start; index < end + 1; index++) {
+    const tile = document.querySelector(`#tile${index}`);
+    tile.classList.replace("sky", tile);
+  }
+}
+
+ceangeTiles(2501, 3000, "rocks");
+
+ceangeTiles(2001, 2500, "dirt");
+
+treeplace = [1925, 1825, 1725, 1625, 1525];
+
+for (let num of treeplace) {
+  const tile = document.querySelector(`#tile${num}`);
+  tile.classList.replace("sky", "tree");
 }
