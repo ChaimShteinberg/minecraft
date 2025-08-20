67c7a09bd94f7d550709082fc76e632f517e66a2
mendi eisenbach <menachemaiz.gmail.com>
2025-08-20 13:05:05 +0300

    Basic tool functions and tool selection listening event

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,15 @@ for (let index = 0; index < 3000; index++) {
     div.id = `tile${index}`
     main.append(div)
 }
+
+document.getElementById('axe').addEventListener('click', () => {
+    selectTool('axe');
+});
+
+document.getElementById('pickaxe').addEventListener('click', () => {
+    selectTool('pickaxe');
+});
+
+document.getElementById('shovel').addEventListener('click', () => {
+    selectTool('shovel');
+});
