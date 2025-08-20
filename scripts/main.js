f9280d02ff88db158f34cc2682a5cc0de6c666ac
Chaim Shteinberg <a0533197133@gmail.com>
2025-08-20 12:35:59 +0300

    create function to replace tile

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,14 @@ const main = document.querySelector("main")
 
 for (let index = 0; index < 3000; index++) {
     const div = document.createElement("div")
-    div.classList = "tile"
+    div.classList.add("tile", "sky")
     div.id = `tile${index}`
     main.append(div)
+}
+
+function ceangeTiles(start, end, tile){
+    for (let index = start; index < end +1 ; index++) {
+        const div = document.querySelector(`#tile${index}`)  
+        div.classList.replace("sky", tile)      
+    }
 }
