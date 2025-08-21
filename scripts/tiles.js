83f243b559d943adf51c8bc963d2fe7393cfa64c
elazar matan <elazarmata8@gmail.com>
2025-08-21 12:35:39 +0300

    Refactor: Move tool logic to tileAfterTool.js
    This commit moves the logic for handling tool interactions with tiles from `tiles.js` to a new file, `tileAfterTool.js`. This improves code organization and makes `tiles.js` more focused on handling click events.

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -1,20 +1,8 @@
-import { selectedTool } from "./tools.js";
- 
+import { select } from "./main.js";
+import { tileAfterTool } from "./tileAfterTool.js";
+
 export const handleClick = (e) => {
-  const typeClass = e.target.classList;
-  if (typeClass.contains("dirt")) {
-    if (selectedTool === "shovel") {
-      e.target.classList.replace("dirt", "sky");
-    }
-  }
-  if(typeClass.contains("tree")){
-    if(selectedTool === "axe"){
-        e.target.classList.replace('tree','sky')
-    }
-  }
-  if(typeClass.contains("rocks")){
-    if(selectedTool === 'pickaxe'){
-        e.target.classList.replace('rocks','sky')
-    }
+  if (select === "tool") {
+    tileAfterTool(e);
   }
 };
